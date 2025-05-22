145005f9d6d9bfa2320129b5c6a1c3ab53b2a610
Saad Chowdhury <saad.c10@gmail.com>
2025-05-21 22:41:07 -0400

    Refactor views for image upload, cleanup, and bugfix

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.contrib import messages as django_messages
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect 
 from django.http import HttpResponse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse
@@ -8,90 +8,75 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import UserManager
-
-from .models import Post, Photo, Comment, Like, User, UserDescription, Message, UserPhoto
-from .forms import PostForm, PhotoForm, CommentForm  # Add UserPhotoForm if you have it!
+from .models import PostForm, PhotoForm
 
 import uuid
 import boto3
+
+from .models import Post, Photo, Comment, Like, User, UserDescription, Message, UserPhoto
+from .forms import CommentForm
 
 S3_BASE_URL = 'https://s3-us-east-2.amazonaws.com/'
 BUCKET = 'garageguru57'
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, 'home.html', {'posts': posts})
+    return render(request, 'home.html', { 'posts': posts })
+
+@login_required
+def messages(request):
+    received_messages = Message.objects.filter(recipient=request.user)
+    return render(request, 'messages.html', {'messages': received_messages})
 
 @login_required
 def profile(request, user_id):
     posts = Post.objects.filter(user=user_id)
-    user_profile = User.objects.filter(id=user_id).values()[0]
-    bio = UserDescription.objects.filter(user_id=user_id).first()
-    bio = bio if bio else {'description': 'No User Bio'}
-    profile_photo = UserPhoto.objects.filter(user_id=user_id).first()
-    return render(request, 'profile.html', {
-        'posts': posts,
-        'user_profile': user_profile,
-        'bio': bio,
-        'profile_photo': profile_photo,
-    })
+    user_profile = User.objects.filter(id = user_id).values()[0]
+    if UserDescription.objects.filter(user_id=user_id).exists():
+        bio = UserDescription.objects.filter(user_id=user_id).values()[0]
+    else:
+        bio = {
+            'description': 'No User Bio'
+        }
+    if UserPhoto.objects.filter(user_id=user_id).exists():
+        profile_photo = UserPhoto.objects.filter(user_id=user_id).values()[0]
+    else:
+        profile_photo = []
+    return render(request, 'profile.html', {'posts': posts, 'user_profile': user_profile, 'bio': bio, 'profile_photo': profile_photo})
 
 @login_required
 def posts_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
+    post = Post.objects.get(id=post_id)
     comment_form = CommentForm()
-    return render(request, 'posts/detail.html', {'post': post, 'comment_form': comment_form})
+    return render(request, 'posts/detail.html', { 'post': post, 'comment_form': comment_form })
 
-# --- POSTS CREATE with photo upload ---
-@login_required
-def posts_create(request):
-    error_message = ''
-    if request.method == 'POST':
-        form = PostForm(request.POST)
-        photo_form = PhotoForm(request.POST, request.FILES)
-        if form.is_valid() and photo_form.is_valid():
-            post = form.save(commit=False)
-            post.user = request.user
-            post.save()
-            photo = photo_form.save(commit=False)
-            photo.post = post
-            photo.save()
-            return redirect('detail', post_id=post.id)
-        else:
-            error_message = 'Invalid new post'
-    else:
-        form = PostForm()
-        photo_form = PhotoForm()
-    context = {'form': form, 'photo_form': photo_form, 'error_message': error_message}
-    return render(request, 'posts/create.html', context)
-
-# --- ADD PHOTO to existing post ---
 @login_required
 def add_photo(request, post_id):
-    if request.method == 'POST':
-        form = PhotoForm(request.POST, request.FILES)
-        if form.is_valid():
-            photo = form.save(commit=False)
-            photo.post_id = post_id
-            photo.save()
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        key = 'garageguru/' + uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            Photo.objects.create(url=url, post_id=post_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3:', e)
     return redirect('detail', post_id=post_id)
 
-# --- PROFILE PHOTO update (optional, only if you have UserPhotoForm) ---
 @login_required
-def user_photo_update(request, user_id):
-    # If you don't have UserPhotoForm, you can still do this with the regular file field
-    if request.method == 'POST':
-        form = PhotoForm(request.POST, request.FILES)
-        if form.is_valid():
-            user_photo, created = UserPhoto.objects.get_or_create(user_id=user_id)
-            user_photo.url = form.cleaned_data['url']  # or 'image', depending on your model
-            user_photo.save()
-    return redirect('profile', user_id=user_id)
+def delete_photo(request, post_id, photo_id):
+    photo = Photo.objects.get(id=photo_id)
+    photo.delete()
+    return redirect('detail', post_id=post_id)
 
 @login_required
 def search_by_hashtag(request, hashtag):
     posts = Post.objects.filter(description__icontains=f'#{hashtag}')
-    context = {'hashtag': hashtag, 'posts': posts}
+    context = {
+        'hashtag': hashtag,
+        'posts': posts,
+    }
     return render(request, 'search_results.html', context)
 
 def signup(request):
@@ -109,44 +94,40 @@ def signup(request):
     return render(request, 'registration/signup.html', context)
 
 @login_required
-def add_comment(request, post_id, user_id):
-    form = CommentForm(request.POST)
-    if form.is_valid():
-        new_comment = form.save(commit=False)
-        new_comment.post_id = post_id
-        new_comment.user_id = user_id
-        new_comment.save()
-    return redirect('detail', post_id=post_id)
+def posts_create(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            newPost = form.save()
+            # Handle photo upload from 'photo-file'
+            photo_file = request.FILES.get('photo-file')
+            if photo_file:
+                s3 = boto3.client('s3')
+                key = 'garageguru/' + uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+                try:
+                    s3.upload_fileobj(photo_file, BUCKET, key)
+                    url = f"{S3_BASE_URL}{BUCKET}/{key}"
+                    Photo.objects.create(url=url, post=newPost)
+                except Exception as e:
+                    print('An error occurred uploading file to S3:', e)
+            return redirect('detail', post_id=newPost.id)
+        else:
+            error_message = 'Invalid new post'
+    form = PostForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'posts/create.html', context)
 
-@login_required
-def delete_comment(request, post_id, comment_id):
-    comment = get_object_or_404(Comment, id=comment_id)
-    comment.delete()
-    return redirect('detail', post_id=post_id)
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['description']
+    success_url = '/'
 
-@login_required
-def add_like(request, post_id, user_id):
-    like, created = Like.objects.get_or_create(post_id=post_id, user_id=user_id)
-    if not created:
-        like.delete()
-    return redirect('home')
-
-def add_like_detail(request, post_id, user_id):
-    like, created = Like.objects.get_or_create(post_id=post_id, user_id=user_id)
-    if not created:
-        like.delete()
-    return redirect('detail', post_id=post_id)
-
-def likes_detail(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
-    return render(request, 'likes/detail.html', {'post': post})
-
-# -- MESSAGING SYSTEM: (keeps your inbox & send views) --
-@login_required
-def messages(request):
-    # This shows inbox of messages where you are recipient
-    received_messages = Message.objects.filter(recipient=request.user)
-    return render(request, 'messages.html', {'messages': received_messages})
+class PostDelete(LoginRequiredMixin, DeleteView):
+    model = Post
+    success_url = '/'
 
 @login_required
 def send_message(request):
@@ -159,14 +140,67 @@ def send_message(request):
             django_messages.error(
                 request, f'User with username {recipient_username} does not exist.')
             return redirect('send_message')
-        # Create new Message object
         message = Message(sender=request.user, recipient=recipient, content=content)
         message.save()
         django_messages.success(request, 'Message sent successfully!')
         return redirect('messages')
     return render(request, 'send_message.html')
 
-# -- Optional: username/bio update views, etc. (keep as you need) --
+@login_required
+def add_comment(request, post_id, user_id):
+    form = CommentForm(request.POST)
+    if form.is_valid():
+        new_comment = form.save(commit=False)
+        new_comment.post_id = post_id
+        new_comment.user_id = user_id
+        new_comment.save()
+    return redirect('detail', post_id=post_id)
+
+@login_required
+def delete_comment(request, post_id, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    comment.delete()
+    return redirect('detail', post_id=post_id)
+
+@login_required
+def add_like(request, post_id, user_id):
+    like_list = []
+    idx = 0
+    for x in Like.objects.filter(post_id=post_id):
+        like_list.append(Like.objects.filter(post_id=post_id).values("user_id")[idx]['user_id'])
+        idx += 1
+
+    if user_id in like_list:
+        Like.objects.filter(post_id=post_id, user_id=user_id).delete()
+    else:
+        new_like = Like()
+        new_like.post_id = post_id
+        new_like.user_id = user_id
+        new_like.save()
+
+    return redirect('home')
+
+def add_like_detail(request, post_id, user_id):
+    like_list = []
+    idx = 0
+    for x in Like.objects.filter(post_id=post_id):
+        like_list.append(Like.objects.filter(post_id=post_id).values("user_id")[idx]['user_id'])
+        idx += 1
+
+    if user_id in like_list:
+        Like.objects.filter(post_id=post_id, user_id=user_id).delete()
+    else:
+        new_like = Like()
+        new_like.post_id = post_id
+        new_like.user_id = user_id
+        new_like.save()
+
+    return redirect('detail', post_id=post_id)
+
+def likes_detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    return render(request, 'likes/detail.html', { 'post': post })
+
 def username_update(request, user_id):
     return render(request, 'main_app/username_form.html')
 
@@ -192,3 +226,21 @@ def bio_updated(request, user_id):
         this_user.description = request.POST['description']
         this_user.save()
         return redirect('profile', user_id=user_id)
+
+def user_photo_update(request, user_id):
+    user_photo_file = request.FILES.get('user_photo-file', None)
+    if user_photo_file:
+        s3 = boto3.client('s3')
+        key = 'garageguru/' + uuid.uuid4().hex[:6] + user_photo_file.name[user_photo_file.name.rfind('.'):]
+        try:
+            s3.upload_fileobj(user_photo_file, BUCKET, key)
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            if UserPhoto.objects.filter(user_id=user_id).exists():
+                new_photo = UserPhoto.objects.get(user_id=user_id)
+                new_photo.url = url
+                new_photo.save()
+            else:
+                UserPhoto.objects.create(url=url, user_id=user_id)
+        except Exception as e:
+            print('An error occurred uploading file to S3:', e)
+    return redirect('profile', user_id=user_id)
