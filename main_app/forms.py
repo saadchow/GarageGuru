de497725c4eb6f406457fd15cecdaf16a7a0d686
Saad Chowdhury <saad.c10@gmail.com>
2025-05-21 22:32:14 -0400

    Refactor views for image upload, cleanup, and bugfix

--- main_app/forms.py
+++ main_app/forms.py
@@ -1,7 +1,22 @@
-from django.forms import ModelForm
-from .models import Comment
+from django import forms
+from .models import Post, Photo, UserPhoto, Comment
 
-class CommentForm(ModelForm):
-  class Meta:
-    model = Comment
-    fields = ['comment']
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post 
+        fields = ["description"]
+
+class PhotoForm(forms.ModelForm):
+    class Meta:
+        model = Photo
+        fields = ["image"]
+
+class UserPhotoForm(forms.ModelForm):
+    class Meta:
+        model = UserPhoto
+        fields = ["image"]
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["comment"]
