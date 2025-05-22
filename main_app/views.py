4f9869f7cd53160421c25fe7ee8d836b3c86e16b
Saad Chowdhury <saad.c10@gmail.com>
2025-05-21 22:50:03 -0400

    Refactor views for image upload, cleanup, and bugfix

--- main_app/views.py
+++ main_app/views.py
@@ -8,7 +8,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.models import UserManager
-from .models import PostForm, PhotoForm
 from .forms import PostForm, PhotoForm
 
 import uuid
