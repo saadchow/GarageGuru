58e73b8d62e6c32b5dec38eb0d3f888bc458d17e
Saad Chowdhury <109829705+saadchow@users.noreply.github.com>
2025-06-09 21:51:49 -0400

    Update settings.py

--- GarageGuru/settings.py
+++ GarageGuru/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nl5-t$qi!@-yp$ddzght-ih7)^2x%0aj_!(y)sl4^=fg*@oy2k'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
