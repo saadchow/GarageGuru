78475c63f26e15386b3f6d4c1fea699300a8b847
Saad Chowdhury <109829705+saadchow@users.noreply.github.com>
2025-06-09 22:00:23 -0400

    Update settings.py

--- GarageGuru/settings.py
+++ GarageGuru/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
+import os
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -31,8 +34,6 @@ import environ
 environ.Env()
 environ.Env.read_env()
 
-import dj_database_url
-import os
 
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
